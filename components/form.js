d7290862b9cc07d60eedbb0cd5f6047036bed6fb
Eric Frey <ericlfrey@gmail.com>
2023-11-14 19:29:45 -0600

    Simple edit

--- components/form.js
+++ components/form.js
@@ -2,27 +2,27 @@ import { houses, students } from '../utils/data/studentData';
 import { createId, renderToDOM } from '../utils/helpers';
 import studentsOnDom from './studentsOnDom';
 
-const sortStudent = (e) => {
-  e.preventDefault();
-  const sortingHat = houses[Math.floor(Math.random() * houses.length)];
-
-  if (e.target.id === 'sorting') {
-    const student = document.querySelector('#student-name');
-
-    // create the new student object
-    students.push({
-      id: createId(students),
-      name: student.value,
-      house: sortingHat.house,
-      crest: sortingHat.crest
-    });
-
-    student.value = ''; // reset value of input
-    studentsOnDom('#students', students);
-  }
-};
-
 const form = () => {
+  const sortStudent = (e) => {
+    e.preventDefault();
+    const sortingHat = houses[Math.floor(Math.random() * houses.length)];
+
+    if (e.target.id === 'sorting') {
+      const student = document.querySelector('#student-name');
+
+      // create the new student object
+      students.push({
+        id: createId(students),
+        name: student.value,
+        house: sortingHat.house,
+        crest: sortingHat.crest
+      });
+
+      student.value = ''; // reset value of input
+      studentsOnDom('#students', students);
+    }
+  };
+
   const domString = `<form id="sorting" class="d-flex flex-column form-floating ">
     <input
     type="text"
